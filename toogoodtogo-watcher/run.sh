9bf8ab4b4b784e8fe57c9fec2e426725950e26eb
Mark Lagendijk <mark@lagendijk.nl>
2025-05-24 20:34:11 +0200

    Try

--- toogoodtogo-watcher/run.sh
+++ toogoodtogo-watcher/run.sh
@@ -1,7 +1,9 @@
 ﻿#!/usr/bin/env bashio
 
-bashio::log.info "Starting toogoodtogo-watcher"
+bashio::log.info "starting toogoodtogo-watcher"
 
 cd /
 export CONFIG=$(bashio::config 'config')
 node node_modules/toogoodtogo-watcher watch --config="${CONFIG}"
+
+bashio::log.error "toogoodtogo-watcher stopped"
