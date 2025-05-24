56e7e9099aa16c659239891349d70df19cef2355
Mark Lagendijk <mark@lagendijk.nl>
2025-05-24 19:14:28 +0200

    More

---
+++ toogoodtogo-watcher/rootfs/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bashio
+
+export CONFIG=$(bashio::config 'config')
+
+bashio::log.info "Starting toogoodtogo-watcher"
+cd /
+exec node node_modules/toogoodtogo-watcher watch
--- toogoodtogo-watcher/run.sh
+++
@@ -1,6 +0,0 @@
-﻿#!/usr/bin/with-contenv bashio
-set +u
-
-export CONFIG=$(bashio::config 'config')
-
-bashio::log.info "Starting node-toogoodtogo-watcher"
