090c476d862d07588903428b82900c848dd4ed70
Mark Lagendijk <mark@lagendijk.nl>
2025-05-26 20:06:13 +0200

    Fix json

--- toogoodtogo-watcher/run.sh
+++ toogoodtogo-watcher/run.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bashio
 
-echo "Test!"
-
 bashio::log.info "starting toogoodtogo-watcher"
 
 cd /
 export TELEGRAM_BOT_TOKEN=$(bashio::config 'telegramBotToken')
-export CONFIG=$(echo {"notifications":{"telegram":{"enabled":true,"botToken":"$TELEGRAM_BOT_TOKEN"}}})
+export CONFIG=$(echo '{"notifications":{"telegram":{"enabled":true,"botToken":"'$TELEGRAM_BOT_TOKEN'"}}}')
 
 node node_modules/toogoodtogo-watcher watch --config="${CONFIG}"
 
