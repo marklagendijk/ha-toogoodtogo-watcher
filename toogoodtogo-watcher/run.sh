af1eacc1e748d924a54c9d2dfd2e6f76e7020764
Mark Lagendijk <mark@lagendijk.nl>
2025-06-28 00:00:05 +0200

    Use addon_config

--- toogoodtogo-watcher/run.sh
+++ toogoodtogo-watcher/run.sh
@@ -3,8 +3,6 @@
 bashio::log.info "starting toogoodtogo-watcher"
 
 cd /
-export TELEGRAM_BOT_TOKEN=$(bashio::config 'telegramBotToken')
-export CONFIG=$(echo '{"notifications":{"telegram":{"enabled":true,"botToken":"'$TELEGRAM_BOT_TOKEN'"}}}')
-export TOOGOODTOGO_CONFIG_DIR=/data/
 
-node node_modules/toogoodtogo-watcher watch --config="${CONFIG}"
+export TOOGOODTOGO_CONFIG_DIR=/config
+node node_modules/toogoodtogo-watcher watch
