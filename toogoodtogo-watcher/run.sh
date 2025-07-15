29e4dded82ff239d2985dccf15c81c8f467b704c
Mark Lagendijk <mark@lagendijk.nl>
2025-07-15 20:11:37 +0200

    Move MQTT config and TGTG email to addon options

--- toogoodtogo-watcher/run.sh
+++ toogoodtogo-watcher/run.sh
@@ -6,4 +6,11 @@ bashio::log.info "Reading config file from /addon_configs/2a12c4fb_node-toogoodt
 cd /
 
 export TOOGOODTOGO_CONFIG_DIR=/config
-node node_modules/toogoodtogo-watcher watch
+
+export TGTG_EMAIL=$(bashio::config 'tgtgEmail')
+export MQTT_URL=$(bashio::config 'mqtt.url')
+export MQTT_USERNAME=$(bashio::config 'mqtt.username')
+export MQTT_PASSWORD=$(bashio::config 'mqtt.password')
+export CONFIG=$(echo '{"api":{"credentials":{"email":"'$TGTG_EMAIL'"}},"notifications":{"mqtt":{"enabled":true,"url":"'$MQTT_URL'","username":"'$MQTT_USERNAME'","password":"'$MQTT_PASSWORD'"}}}')
+
+node node_modules/toogoodtogo-watcher watch --config="${CONFIG}"
